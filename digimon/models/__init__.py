e1ad9da1535cf800570f6af71655762b016c5e93
Natthawut Suksai <owen55660@gmail.com>
2024-08-06 20:36:22 +0700

    add .env sample

--- digimon/models/__init__.py
+++ digimon/models/__init__.py
@@ -31,7 +31,7 @@ async def create_all():
         await conn.run_sync(SQLModel.metadata.create_all)
 
 
-async def get_session() -> AsyncSession:
+async def get_session() -> AsyncSession: # type: ignore
     async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     async with async_session() as session:
         yield session   
